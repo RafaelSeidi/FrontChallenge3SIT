8a94d80d9e3a792bacaa1dd4eeb914bca4dc5758
RafaelSeidi <rafasuzukitomita@gmail.com>
2024-09-27 02:12:51 -0300

    Listando usuarios funcionando

---
+++ tela-admin/consulta-func/script.js
@@ -0,0 +1,44 @@
+listar();
+
+function listar() {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+
+    const requestOptions = {
+        headers: myHeaders,
+        method: "GET",
+        redirect: "follow"
+    };
+
+    fetch("http://localhost:8094/usuario/listar", requestOptions)
+        .then((response) => response.json())
+        .then((json) => {
+            if(json.length !== 0){
+                console.log(json)
+                construirTabela(json);
+            } else {
+                alert('Consulta retornou vazia');
+            }
+        })
+        .catch((error) => console.error(error));
+}
+
+function construirTabela(json){
+    var table = document.getElementById('tabela');
+
+    for (var i = 0; i < json.length; i++){
+        var row = `<tr>
+                        <th scope="row">${json[i].usuarioId}</th>
+                        <td>${json[i].firstName} ${json[i].lastName}</td>
+                        <td>${json[i].email}</td>
+                        <td>${json[i].telefone}</td>
+                        <td>
+                            <div class="box-op">
+                                <a href="" class="custom-icon"><i class="fi fi-bs-cross"></i></a>
+                            </div>
+                        </td>
+                   </tr>`
+        table.innerHTML += row;
+    }
+}
